838feead48f8369d9436d3021dab2e3a109eed59
jaeyun <wodbs0723@ut.ac.kr>
2024-01-03 23:47:33 +0900

    2024/01/03 저녁

---
+++ code.plus_기초/src/code/plus_브루트포스/문제_15649_N과M_1.java
@@ -0,0 +1,19 @@
+package code.plus_브루트포스;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 문제_15649_N과M_1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		StringTokenizer st = new StringTokenizer(s, " ");
+		int n = Integer.parseInt(st.nextToken()); // 1부터 n 까지의 자연수중
+		int m = Integer.parseInt(st.nextToken()); // 중복없이 M개를 고른 수열
+		StringBuilder sb = new StringBuilder();
+		
+		System.out.println(sb);
+	}
+
+}
--- code.plus_기초/src/code/plus_브루트포스/문제_2309_일곱난쟁이.java
+++ code.plus_기초/src/code/plus_브루트포스/문제_2309_일곱난쟁이.java
@@ -1,16 +1,18 @@
 package code.plus_브루트포스;
 
-import java.io.*;
+import java.util.*;
 
 public class 문제_2309_일곱난쟁이 {
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		int height[] = new int[9];
 		for (int i = 0; i < height.length; i++) {
-			height[i] = br.read();
+			height[i] = sc.nextInt();
 		}
-		System.out.println(height[1]);
+		Arrays.sort(height);
+		int sum = 0;
+		
 	}
 
 }
