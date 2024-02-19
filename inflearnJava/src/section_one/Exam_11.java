ca1d50fdbcb7070794d94a7959c7699bf7955d7c
jaeyun <wodbs0723@ut.ac.kr>
2024-02-19 22:40:24 +0900

    2024/02/19 저녁

---
+++ inflearnJava/src/section_one/Exam_11.java
@@ -0,0 +1,19 @@
+package section_one;
+
+import java.util.Scanner;
+
+public class Exam_11 {
+	
+	public static int solution(String str) {
+		int answer = 0;
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(solution(str));
+
+	}
+
+}
