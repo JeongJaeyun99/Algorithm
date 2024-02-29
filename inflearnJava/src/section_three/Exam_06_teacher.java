9b745e0896c69fd274129822a1a7668bec44c57a
jaeyun <wodbs0723@ut.ac.kr>
2024-02-29 23:59:02 +0900

    2024/02/29 저녁

---
+++ inflearnJava/src/section_three/Exam_06_teacher.java
@@ -0,0 +1,19 @@
+package section_three;
+
+import java.util.Scanner;
+
+public class Exam_06_teacher {
+
+	public static int solution(int n) {
+		int cnt = 0;
+		
+		return cnt;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(solution(n));
+	}
+
+}
