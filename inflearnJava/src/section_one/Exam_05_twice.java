24850e0659b2a6c852fb0f7684268af916a566c2
jaeyun <wodbs0723@ut.ac.kr>
2024-05-03 16:43:16 +0900

    2024/05/03 코테 준비

---
+++ inflearnJava/src/section_one/Exam_05_twice.java
@@ -0,0 +1,23 @@
+package section_one;
+
+import java.util.Scanner;
+
+public class Exam_05_twice {
+	
+	public static String solution(String str) {
+		String answer = "";
+		for (int i = 0; i < str.length(); i++) {
+			if(Character.isAlphabetic(str.charAt(i))) {
+				
+			}
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(solution(str));
+	}
+
+}
